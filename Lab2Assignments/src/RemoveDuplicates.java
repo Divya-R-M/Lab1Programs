356de0573fb7e4384240fb39f13d9c17669a70e4
Divya-R-M <73276613+Divya-R-M@users.noreply.github.com>
2020-10-30 12:16:26 +0530

    Update RemoveDuplicates.java

--- Lab2Assignments/src/RemoveDuplicates.java
+++ Lab2Assignments/src/RemoveDuplicates.java
@@ -4,42 +4,54 @@
  * Desc : To remove the duplicate elements in the array and sort it in descending order.
  */
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
-public class RemoveDuplicates {
+public class Duplicate {
 
 	public static void main(String[] args) {
-		int n;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number of elements in array : ");
-		n = sc.nextInt();
-		int arr[] = new int[n];
-		System.out.println("Enter the elements of array : ");
-		for (int i = 0; i < n; i++) {
-			arr[i] = sc.nextInt();
-		}
-		Arrays.sort(arr); // sorting the array
-		System.out.println("The new array is "); // printing the new array
-		for (int i = n - 1; i > 0; i--) {
-			System.out.println(arr[i] + " ");
-		}
+		
+		Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array");
+        int size=sc.nextInt();
+        int inputArray[]=new int[size];                                //Declaring and initializing of integer array
+        System.out.println("Enter the elements in the array");
+        for(int i=0;i<size;i++) {
+        	inputArray[i]=sc.nextInt();
+        }
+        int resultantArray[]=modifyArray(inputArray);
+        System.out.println("The sorted array is");
+        for(int i=resultantArray.length-1;i>=0;i--) {
+        	System.out.println(resultantArray[i]);   
+        }
+        sc.close();
 	}
 
-	// ---------using temporary array to store the unique elements---------
-	private static int[] modifyArray(int[] arr, int total) {
-		int j = 0;
-		int temp[] = new int[total];
-		for (int i = 0; i < total - 1; i++) {
-			if (arr[i] != arr[i + 1]) {
-				temp[j] = arr[i];
-				j++;
-			}
-		}
-		temp[j] = arr[total - 1];
-		// --------to return the new array-------
-		for (int i = 0; i < total; i++) {
-			arr[i] = temp[i];
-		}
-		return arr;
+	public static int[] modifyArray(int[] arr)             //Remove the duplicate elements and sorting in descending order 
+	{
+		int len=arr.length;
+		int[] tempArray = new int[len];  
+        int index = 0, count=0;  
+        for (int itr=0; itr<len-1; itr++){  
+            if (arr[itr] != arr[itr+1]){  
+                tempArray[index++] = arr[itr];  
+            } 
+            else{
+            	count++;
+            }
+         }  
+        tempArray[index++] = arr[len-1];     
+        int result[]=new int[len-count];                      // Creating and adding elements into the new array
+        int j=0;
+        for(int i=0;i<len;i++) {
+        	if(tempArray[i]<=0) {
+        		continue;
+        	}
+        	else
+        		result[j++]=tempArray[i];
+        }
+        Arrays.sort(result);                                 // Sorting
+        return result;
 	}
+
 }
